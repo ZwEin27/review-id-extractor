d67becf9a7838714a3d4220b82453ca4ee7abf73
ZwEin <zwein27@gmail.com>
2016-07-25 11:18:10 -0400

    catch case Highly reviewed on , 178352

--- digride.py
+++ digride.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-22 17:52:30
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-25 11:03:02
+# @Last Modified time: 2016-07-25 11:18:02
 
 import re
 
@@ -16,14 +16,19 @@ punctuations = r'\!\"\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\
 #   Regular Expression
 ######################################################################
 
+reg_gap_word = r'(?:[\s'+punctuations+r']*?[a-z].*[a-z][\s'+punctuations+r']*?)'
+
+reg_fc_word = reg_gap_word+r'{,3}'+r'(?:review)'+reg_gap_word+r'{,3}'
+reg_fc_simble =  r'.{,5}?[#]'
 reg_fc_ter = r'(?:\bt[e3]r\s*?|t\s*?[e3]?\s*?r|\bt\b|\be\b|\br\b)'
 reg_fc_id = r'i[\s'+punctuations+r']{,5}?d'
 
 reg_back_check = r'(?!['+punctuations+r']*?\d)'
-reg_target = r'.{,5}?#?.{,2}?\d{6}'
+reg_target = r'.{,2}?\d{6}'
 
 reg_rid = [
-    r'(?:'+reg_target+reg_back_check+r')',
+    r'(?:'+reg_fc_word+reg_target+reg_back_check+r')',
+    r'(?:'+reg_fc_simble+reg_target+reg_back_check+r')',
     r'(?:\b'+reg_fc_ter+reg_target+reg_back_check+r')',
     r'(?:\b'+reg_fc_id+reg_target+reg_back_check+r')',
     r'(?:\b'+reg_fc_ter+r'.{,5}'+reg_fc_id+reg_target+reg_back_check+r')'
@@ -50,8 +55,10 @@ class DIGRIDE(object):
 
 if __name__ == '__main__':
     # text = "My I.D.267101"
-    text = "Reviewed #229512 T.e. R"
+    # text = "Reviewed #229512 T.e. R"
     # text = "Hey guys! I'm Heidi!!! I am a bubbly, busty, blonde massage therapist and only provide the most sensual therapeutic experience! I love what I do and so will YOU!!! I am always learning new techniqes and helping other feel relaxed. Just send Me an email and lets meet!!!  I am reviewed! #263289 \nheidishandsheal@gmail.com"
     # text = "Reviewed TER 202567 "
     # text = "EXCEPTION TRiD:263865.Call  404-599-8674"
+    # text = "VIP. Sexy Escort Girls. High Class Pune Escorts agency CALL ROBIN- 096997 37222 / 09767 303000 "
+    text = "Highly reviewed on , 178352 "
     print DIGRIDE.extract(text)
