79dfd1cc34a81d4432f2b64e3c51a8cb7b5665fd
ZwEin <zwein27@gmail.com>
2016-07-25 11:53:26 -0400

    add support for multiple targets

--- digride.py
+++ digride.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-22 17:52:30
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-25 11:38:40
+# @Last Modified time: 2016-07-25 11:49:14
 
 import re
 
@@ -10,7 +10,7 @@ import re
 #   Constant
 ######################################################################
 
-punctuations = r'\!\"\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~'
+punctuations = r'\!\"\!\"\#\$\%\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~'
 keywords = [
     'review',
     'reviewed'
@@ -31,14 +31,15 @@ reg_fc_simble =  r'.{,5}?[#]'
 reg_fc_ter = r'(?:\bt[e3]r\s*?|t\s*?[e3]?\s*?r|\bt\b|\be\b|\br\b)'
 reg_fc_id = r'i[\s'+punctuations+r']{,5}?d'
 
-reg_back_check = r'(?!['+punctuations+r']*?\d)'
-reg_target = r'.{,2}?\d{6}'
+reg_back_check = r'(?!['+punctuations+r']*?\d{6})'
+
+reg_target = r'.{,2}?(?:\d{6}[\s\&]*)+?'
 
 reg_rid = [
-    # r'(?:'+reg_fc_simble+reg_target+reg_back_check+r')',
-    # r'(?:\b'+reg_fc_ter+reg_target+reg_back_check+r')',
-    # r'(?:\b'+reg_fc_id+reg_target+reg_back_check+r')',
-    # r'(?:\b'+reg_fc_ter+r'.{,5}'+reg_fc_id+reg_target+reg_back_check+r')',
+    r'(?:'+reg_fc_simble+reg_target+reg_back_check+r')',
+    r'(?:\b'+reg_fc_ter+reg_target+reg_back_check+r')',
+    r'(?:\b'+reg_fc_id+reg_target+reg_back_check+r')',
+    r'(?:\b'+reg_fc_ter+r'.{,5}'+reg_fc_id+reg_target+reg_back_check+r')',
     r'(?:'+reg_fc_word_prev+reg_target+reg_back_check+r')'
 ]
 re_rid = re.compile(r'(?:'+r'|'.join(reg_rid)+r')', re.IGNORECASE)
@@ -55,12 +56,13 @@ class DIGRIDE(object):
 
     @staticmethod
     def extract(text):
+        ans = []
         text = ' '.join([_.strip() for _ in re_tokenize.split(text) if _.strip() != ''])
         # print text.encode('ascii', 'ignore')
-        ans = re_rid.findall(text)
-        # print ans
-        ans = [re_digits.search(_).group(0) for _ in ans]
-
+        potentials = re_rid.findall(text)
+        # print potentials
+        for p in potentials:
+            ans += re_digits.findall(p)
         return ans
 
 
@@ -69,7 +71,8 @@ if __name__ == '__main__':
     # text = "Reviewed #229512 T.e. R"
     # text = "Hey guys! I'm Heidi!!! I am a bubbly, busty, blonde massage therapist and only provide the most sensual therapeutic experience! I love what I do and so will YOU!!! I am always learning new techniqes and helping other feel relaxed. Just send Me an email and lets meet!!!  I am reviewed! #263289 \nheidishandsheal@gmail.com"
     # text = "Reviewed TER 202567 "
-    # text = "EXCEPTION TRiD:263865.Call  404-599-8674"
+    text = "EXCEPTION TRiD:263865.Call  404-599-8674"
     # text = "VIP. Sexy Escort Girls. High Class Pune Escorts agency CALL ROBIN- 096997 37222 / 09767 303000 "
-    text = "Highly reviewed on , 178352 "
+    # text = "Highly reviewed on , 178352 "
+    # text = "HIGHLY REVIEWED id number 195001 & 260255 OUTCALL/INCALL"
     print DIGRIDE.extract(text)
