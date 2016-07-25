63f01ae0b5466302f1e924817207726a198b908d
ZwEin <zwein27@gmail.com>
2016-07-25 10:18:40 -0400

    refine reg front check

--- digride.py
+++ digride.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-22 17:52:30
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-25 10:15:59
+# @Last Modified time: 2016-07-25 10:18:25
 
 import re
 
@@ -16,13 +16,16 @@ punctuations = r'\!\"\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\
 #   Regular Expression
 ######################################################################
 
+reg_fc_ter = r'(?:t\b\s*?e\b\s*?r|\bt\b|\be\b|\br\b)'
+reg_fc_id = r'i[\s'+punctuations+r']{,5}?d'
+
 reg_back_check = r'(?!['+punctuations+r']*?\d))'
 reg_target = r'.{,5}?\d{6}'
 
 reg_rid = [
-    r'(?:\b(?:t\b\s*?e\b\s*?r|\bt\b|\be\b|\br\b)'+reg_target+reg_back_check,
-    r'(?:\bi[\s'+punctuations+r']{,5}?d'+reg_target+reg_back_check,
-    r'(?:\b(?:t\s*?e\s*?r|\bt\b|\be\b|\br\b).{,5}i[\s'+punctuations+r']{,5}?d'+reg_target+reg_back_check
+    r'(?:\b'+reg_fc_ter+reg_target+reg_back_check,
+    r'(?:\b'+reg_fc_id+reg_target+reg_back_check,
+    r'(?:\b'+reg_fc_ter+r'.{,5}'+reg_fc_id+reg_target+reg_back_check
 ]
 re_rid = re.compile(r'(?:'+r'|'.join(reg_rid)+r')', re.IGNORECASE)
 
